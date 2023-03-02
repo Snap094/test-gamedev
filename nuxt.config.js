2695f5178c5cbf2d766e698ee51556e9e2da5aec
nikonchuk.pp <nom100snap@gmail.com>
2023-03-02 09:13:21 +0200

    edited config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    base: 'test-gamedev',
+    mode: 'static',
+    router: {
+        base: 'test-gamedev'
+    },
     css: ["@/assets/styles/style.scss"],
     vite: {
         css: {
