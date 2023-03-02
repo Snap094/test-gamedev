e7ccf97f87c1df351ec80ed2820a58075faa64c8
nikonchuk.pp <nom100snap@gmail.com>
2023-03-02 10:07:39 +0200

    edit path src

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        baseURL: '/test-gamedev/'
+    mode: 'static',
+    router: {
+        base: '/test-gamedev/'
     },
     css: ["@/assets/styles/style.scss"],
     vite: {
