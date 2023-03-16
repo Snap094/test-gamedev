2f3b1cb823ffad57741389cf9f54e40c002e73b7
nikonchuk.pp <nom100snap@gmail.com>
2023-03-16 13:50:42 +0200

    Updates

--- _nuxt/app.config.61d7cc03.js
+++
@@ -1,1 +0,0 @@
-import{z as u,K as s}from"./entry.7ccdeae0.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
---
+++ _payload.js
@@ -0,0 +1,1 @@
+export default {data:{},prerenderedAt:1678967429444}
